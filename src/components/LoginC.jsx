b43f32903f794e635ad0f465db74527606a224db
unknown <juan.cosen2004@gmail.com>
2024-01-20 04:32:51 -0300

    modificacion visual del login

--- src/components/LoginC.jsx
+++ src/components/LoginC.jsx
@@ -1,29 +1,89 @@
 import React from 'react'
 import '../css/LoginC.css'
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
 
-const LoginC = () => {
+function LoginC() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes manejar la lógica de envío del formulario
+    console.log('Correo:', email, 'Contraseña:', password);
+  };
+
   return (
     <>
-        <div className='containerLogin'>
-                <div className='section-2 glass'>
-                  <form className='m-5'>
-                    {/* <img src="https://dbdzm869oupei.cloudfront.net/img/sticker/preview/30760.png" alt="Logo de la veterinaria"
-                    className='imgLogin' /> */}
-                    <div className="mb-3">
-                      <label for="exampleInputEmail1" className="form-label ">Usuario</label>
-                      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                      placeholder=''/>
-                    </div>
-                    <div className="mb-3">
-                      <label for="exampleInputPassword1" className="form-label">Contraseña</label>
-                      <input type="password" className="form-control" id="exampleInputPassword1"/>
-                    </div>
-                    <button type="submit" className="btn btn-primary btn-css">Iniciar Sesion</button>
-                  </form>
-                </div>
+      <div className='body'>
+      <section className='left-section'>
+        <div className='welcome'>
+          <h2>¡Bienvenido al refugio del bienestar animal!,</h2>
+          <p>Aquí, cada ronroneo 
+          y ladrido es una canción de alegría. Permítenos ser parte del viaje 
+          de cuidado y felicidad de tus queridos amigos peludos.</p>
+          <img  className='imgLogin' src="https://cdn-icons-png.flaticon.com/512/7019/7019169.png" 
+          alt=""/>
         </div>
+      </section>
+      <section className='right-section'>
+        <Form className='formLogin' onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Correo Electrónico</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Ingresa tu correo"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Ingresa tu contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Enviar
+          </Button>
+        </Form>
+      </section>
+    </div>
     </>
-  )
+    
+  );
 }
 
-export default LoginC
+/* const LoginC = () => {
+
+
+  return (
+    <div className='body'>
+      <section className='left-section'>
+        <div className='welcome'>
+          <h2>¡Bienvenido al refugio del bienestar animal!,</h2>
+          <p>Aquí, cada ronroneo 
+          y ladrido es una canción de alegría. Permítenos ser parte del viaje 
+          de cuidado y felicidad de tus queridos amigos peludos.</p>
+        </div>
+      </section>
+      <section className='right-section'>
+        
+      </section>
+    </div>
+  );
+};
+ */
+
+export default LoginC 
