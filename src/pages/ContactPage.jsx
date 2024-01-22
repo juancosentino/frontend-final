3732a4f52be20129d4a200dde33d64dc39ce6bb0
unknown <juan.cosen2004@gmail.com>
2024-01-22 17:38:03 -0300

    agregue el componente de contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,14 +2,17 @@ import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import NavbarC from "../components/NavbarC"
+import FooterC from '../components/FooterC';
+import ContactC from '../components/ContactC';
 
 const ContactPage = () => {
   return (
     <>
         <NavbarC/>
-        <Container>
 
-        </Container>
+        <ContactC/>
+
+        <FooterC/>
     </>
     
   )
