70154eded8dd3afa1db24f97c146e0f7cf46447e
unknown <juan.cosen2004@gmail.com>
2024-02-06 22:57:50 -0300

    agregue pagina de admin y sus componentes

---
+++ src/pages/AdminPacientesPage.jsx
@@ -0,0 +1,15 @@
+import NavbarC from "../components/NavbarC"
+import FooterC from "../components/FooterC"
+import AdminPacientes from "../components/AdminPacientes"
+
+const AdminPacientesPage = () => {
+  return (
+    <>
+        <NavbarC/>
+        <AdminPacientes/>
+        <FooterC/>
+    </>
+  )
+}
+
+export default AdminPacientesPage
