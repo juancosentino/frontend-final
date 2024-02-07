2fe2020b7474f74cd8364fee3a28c9536d9b966b
unknown <juan.cosen2004@gmail.com>
2024-02-07 00:06:29 -0300

    actualice archivo app e index

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import HomePage from "./pages/HomePage"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ContactPage from "./pages/ContactPage"
@@ -7,12 +6,21 @@ import WePage from "./pages/WePage"
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from './pages/RegisterPage'
 import PlansPage from './pages/PlansPage'
-
+import ProductPage from "./pages/ProductPage"
+import ProductsPage from "./pages/ProductsPage"
+import UserPage from "./pages/UserPage"
+import AdminPage from "./pages/AdminPage"
+import TurnosPage from "./pages/turnosPage"
+import ProductsAlimento from "./pages/ProductsAlimento"
+import ProductsAccesorios from "./pages/ProductsAccesorios"
+import ProductsJuguetes from "./pages/ProductsJuguetes"
+import AdminPacientesPage from "./pages/AdminPacientesPage"
+import AdminTurnosPage from "./pages/AdminTurnosPage"
 
 const App = () => {
 
   return (
-    <> {/* Router - Routes- Route */}
+    <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage/>} />
@@ -21,6 +29,16 @@ const App = () => {
           <Route path="/login" element={<LoginPage/>}></Route>
           <Route path="/Register" element={<RegisterPage/>}></Route>
           <Route path="/Plans" element={<PlansPage/>}></Route>
+          <Route path="/Product/:id" element={<ProductPage/>}></Route>
+          <Route path="/Products" element={<ProductsPage/>}></Route>
+          <Route path="/User" element={<UserPage/>}></Route>
+          <Route path="/Admin" element={<AdminPage/>}></Route>
+          <Route path="/Turnos" element={<TurnosPage/>} ></Route>
+          <Route path="/Alimento" element={<ProductsAlimento/>} ></Route>
+          <Route path="/Accesorio" element={<ProductsAccesorios/>} ></Route>
+          <Route path="/Juguetes" element={<ProductsJuguetes/>} ></Route>
+          <Route path="/adminPacientes" element={<AdminPacientesPage/>} ></Route>
+          <Route path="/adminTurnos" element={<AdminTurnosPage/>} ></Route>
           <Route path="*" element={<ErrorPage/>} />
         </Routes>
       </BrowserRouter>
