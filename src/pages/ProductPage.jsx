c88c43ead83c3c40c0b4b191d6867fbe68cf60db
unknown <juan.cosen2004@gmail.com>
2024-02-06 23:30:16 -0300

    agregue page de los distintos productos

---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,19 @@
+import NavbarC from "../components/NavbarC"
+import FooterC from "../components/FooterC"
+import ProductC from "../components/ProductC"
+
+
+const ProductPage = () => {
+    return (
+    <>
+        <NavbarC/>
+
+        <ProductC/>
+
+        <FooterC/>
+    </>
+  )
+}
+
+export default ProductPage
+
---
+++ src/pages/ProductsAlimento.jsx
@@ -0,0 +1,15 @@
+import AlimentosC from "../components/AlimentosC"
+import NavbarC from "../components/NavbarC"
+import FooterC from "../components/FooterC"
+
+const ProductsAlimento = () => {
+  return (
+    <>
+        <NavbarC/>
+        <AlimentosC/>
+        <FooterC/>
+    </>
+  )
+}
+
+export default ProductsAlimento
